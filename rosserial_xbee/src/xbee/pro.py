d79e234765e12af5f112403723ddb50e42209874
Robert Cofield <robertgcofield@gmail.com>
2013-08-20 11:29:56 -0500

    likely my final commit for some time.

--- rosserial_xbee/src/xbee/pro.py
+++ rosserial_xbee/src/xbee/pro.py
@@ -22,3 +22,50 @@ class XBeePro(XBeeBase):
   be read from a device syncronously by calling wait_read_frame. For
   asynchronous reads, see the definition of XBeeBase.
   """
+  # Packets which can be sent to an XBee
+  # Format: 
+  #        {name of command:
+  #           [{name:field name, len:field length, default: default value sent}
+  #            ...
+  #            ]
+  #         ...
+  #         }
+  api_commands = {
+    "tx": [
+      {'name':'id',              'len':1,        'default':'\x01'},
+      {'name':'frame_id',        'len':1,        'default':'\x00'},
+      {'name':'dest_addr',       'len':2,        'default':None},
+      {'name':'options',         'len':1,        'default':'\x00'},
+      {'name':'data',            'len':None,     'default':None}
+    ],
+  }
+  
+  # Packets which can be received from an XBee
+  # Format: 
+  #        {id byte received from XBee:
+  #           {name: name of response
+  #            structure:
+  #                [ {'name': name of field, 'len':length of field}
+  #                  ...
+  #                  ]
+  #            parse_as_io_samples:name of field to parse as io
+  #           }
+  #           ...
+  #        }
+  #
+  api_responses = {
+    "\x01": {
+      'name':'rx',
+      'structure': [
+        # {'name':'id',              'len':1,        'default':'\x01'},
+        {'name':'frame_id',        'len':1,        'default':'\x00'},
+        {'name':'dest_addr',       'len':2,        'default':None},
+        {'name':'options',         'len':1,        'default':'\x00'},
+        {'name':'data',            'len':None,     'default':None}
+      ],
+    },
+  }
+  
+  def __init__(self, *args, **kwargs):
+      # Call the super class constructor to save the serial port
+      super(XBeePro, self).__init__(*args, **kwargs)
